e739256ca7fe68b165af1495021c96b406b4ae6c
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-03-08 19:50:58 +0000

    Moved to Truffle v3.

--- test/incrementor.js
+++ test/incrementor.js
@@ -1,91 +1,117 @@
-var getEventsPromise = function (myFilter, count) {
-  return new Promise(function (resolve, reject) {
-    count = count ? count : 1;
-    var results = [];
-    myFilter.watch(function (error, result) {
-      if (error) {
-        reject(error);
-      } else {
-        count--;
-        results.push(result);
-      }
-      if (count <= 0) {
-        resolve(results);
-        myFilter.stopWatching();
-      }
-    });
-  });
-};
+var Incrementor = artifacts.require("./Incrementor.sol");
+
+Extensions = require("../utils/extensions.js");
+Extensions.init(web3, assert);
+
 
 contract('Incrementor', function(accounts) {
 
-  it("should receive constructor event", function() {
+    var owner, incrementor, block0;
 
-    var incrementor = Incrementor.deployed();
-    return getEventsPromise(incrementor.LogValue(
-        {},
-        { fromBlock: 0, toBlock: "latest" }))
-      .then(function (events) {
-        assert.equal(events.length, 1, "should have received 1 event");
-        assert.equal(events[0].args.value.valueOf(), 0, "should be 0 from constructor");
-      });
+    before("should have proper accounts", function() {
+        assert.isAtLeast(accounts.length, 1, "should have at least 1 account");
+        owner = accounts[0];
+        return Extensions.makeSureAreUnlocked([ owner ]);
+    });
 
-  });
+    beforeEach("should deploy an incrementor", function() {
+        return Incrementor.new({ from: owner })
+            .then(created => {
+                incrementor = created;
+                return web3.eth.getTransactionReceiptPromise(created.transactionHash);
+            })
+            .then(receipt => {
+                if (typeof block0 === "undefined") {
+                    block0 = receipt.blockNumber;
+                }
+            });
+    });
 
-  it("should receive next event", function() {
+    it("should receive constructor event", function() {
+        return web3.eth.getTransactionReceiptPromise(incrementor.transactionHash)
+            .then(receipt => {
+                assert.strictEqual(receipt.logs.length, 1, "should have received 1 event");
+                assert.strictEqual(
+                    receipt.logs[0].topics[0],
+                    web3.sha3("LogValue(uint256)"),
+                    "should be the hash of the event");
+                var event0 = incrementor.LogValue().formatter(receipt.logs[0]);
+                assert.strictEqual(
+                    event0.args.value.toNumber(), 0,
+                    "should be 0 from constructor");
+            });
+    });
 
-    var incrementor = Incrementor.deployed();
-    var blockNumber = web3.eth.blockNumber;
-    return incrementor.increaseValue({ from: accounts[0] })
-      .then(function (tx) {
-        return getEventsPromise(incrementor.LogValue(
-          {},
-          { fromBlock: blockNumber + 1, toBlock: "latest" }));
-      })
-      .then(function (events) {
-        assert.equal(events.length, 1, "should have received 1 event");
-        assert.equal(events[0].args.value.valueOf(), 1, "should be the increased value");
-      });
+    it("should receive next event", function() {
+        return incrementor.increaseValue({ from: owner })
+            .then(txObject => {
+                assert.strictEqual(txObject.logs.length, 1, "should have received 1 event");
+                assert.strictEqual(
+                    txObject.logs[0].args.value.toNumber(), 1,
+                    "should be the increased value");
+            });
+    });
 
-  });
+    it("should receive 2 events across blocks", function() {
+        return Promise.all([
+                incrementor.increaseValue({ from: owner }),
+                incrementor.increaseValue({ from: owner })
+            ])
+            .then(txObjects => Extensions.getEventsPromise(incrementor.LogValue(
+                    {},
+                    {
+                        fromBlock: txObjects[0].receipt.blockNumber,
+                        toBlock: txObjects[1].receipt.blockNumber
+                    }),
+                2
+            ))
+            .then(events => {
+                assert.strictEqual(events.length, 2, "should have received those 2 events");
+                assert.strictEqual(
+                    events[0].args.value.toNumber(), 1,
+                    "should be the increased value");
+                assert.strictEqual(
+                    events[1].args.value.toNumber(), 2,
+                    "should be the increased value");
+            });
 
-  it("should receive next 2 events", function() {
+    });
 
-    var incrementor = Incrementor.deployed();
-    var blockNumber = web3.eth.blockNumber;
-    return incrementor.increaseValue({ from: accounts[0] })
-      .then(function (tx) {
-        return incrementor.increaseValue({ from: accounts[0] });
-      })
-      .then(function (tx) {
-        return getEventsPromise(incrementor.LogValue(
-            {},
-            { fromBlock: blockNumber + 1, toBlock: "latest" }),
-          2);
-      })
-      .then(function (events) {
-        assert.equal(events.length, 2, "should have received those 2 events");
-        assert.equal(events[0].args.value.valueOf(), 2, "should be the increased value");
-        assert.equal(events[1].args.value.valueOf(), 3, "should be the increased value");
-      });
-
-  });
-
-  it("should receive all past events", function() {
-
-    var incrementor = Incrementor.deployed();
-    return getEventsPromise(incrementor.LogValue(
-          {},
-          { fromBlock: 0, toBlock: "latest" }),
-        4)
-      .then(function (events) {
-        assert.equal(events.length, 4, "should have received all 4 events");
-        assert.equal(events[0].args.value.valueOf(), 0, "should be the constructor value");
-        assert.equal(events[1].args.value.valueOf(), 1, "should be the increased value");
-        assert.equal(events[2].args.value.valueOf(), 2, "should be the increased value");
-        assert.equal(events[3].args.value.valueOf(), 3, "should be the increased value");
-      });
-
-  });
+    it("should receive past events of all instances", function() {
+        return Extensions.getEventsPromise(web3.eth.filter(
+                    {
+                        topics: [ web3.sha3("LogValue(uint256)") ],
+                        fromBlock: block0,
+                        toBlock: "latest"
+                    }),
+                7)
+            .then(events => {
+                assert.strictEqual(
+                    events.length, 7,
+                    "should have received all 7 events of this contract type");
+                events = events.map(element => incrementor.LogValue().formatter(element));
+                assert.strictEqual(
+                    events[0].args.value.toNumber(), 0,
+                    "should be the constructor value");
+                assert.strictEqual(
+                    events[1].args.value.toNumber(), 0,
+                    "should be the constructor value");
+                assert.strictEqual(
+                    events[2].args.value.toNumber(), 1,
+                    "should be the increased value");
+                assert.strictEqual(
+                    events[3].args.value.toNumber(), 0,
+                    "should be the constructor value");
+                assert.strictEqual(
+                    events[4].args.value.toNumber(), 1,
+                    "should be the increased value");
+                assert.strictEqual(
+                    events[5].args.value.toNumber(), 2,
+                    "should be the increased value");
+                assert.strictEqual(
+                    events[6].args.value.toNumber(), 0,
+                    "should be the constructor value");
+            });
+    });
 
 });
--- test/owned.js
+++ test/owned.js
@@ -1,81 +1,101 @@
-var getEventsPromise = function (myFilter, count) {
-  return new Promise(function (resolve, reject) {
-    count = count ? count : 1;
-    var results = [];
-    myFilter.watch(function (error, result) {
-      if (error) {
-        reject(error);
-      } else {
-        count--;
-        results.push(result);
-      }
-      if (count <= 0) {
-        resolve(results);
-        myFilter.stopWatching();
-      }
-    });
-  });
-};
+var Owned = artifacts.require("./Owned.sol");
+
+Extensions = require("../utils/extensions.js");
+Extensions.init(web3, assert);
 
 contract('Owned', function(accounts) {
 
-  // For the moment, these tests do not work with TestRPC.
-  
-  it("should receive the changed owner", function() {
+    var owner, owner2, owned, block0;
 
-    var owned = Owned.deployed();
-    return owned.setOwner(accounts[1], { from: accounts[0] })
-      .then(function (tx) {
-        return getEventsPromise(owned.OnOwnerChanged(
-          {},
-          { fromBlock: 0, toBlock: "latest" }));
-      })
-      .then(function (events) {
-        assert.equal(events.length, 1, "should have received 1 event");
-        assert.equal(events[0].args.previousOwner, accounts[0], "should be the first account");
-        assert.equal(events[0].args.newOwner, accounts[1], "should be the second account");
-        return owned.setOwner(accounts[0], { from: accounts[1] });
-      })
-      .then(function (tx) {
-        return Promise.all([
-            getEventsPromise(owned.OnOwnerChanged(
-              { previousOwner: accounts[0] },
-              { fromBlock: 0, toBlock: "latest" })),
-            getEventsPromise(owned.OnOwnerChanged(
-              { previousOwner: accounts[1] },
-              { fromBlock: 0, toBlock: "latest" }))
-          ]);
-      })
-      .then(function (events) {
-        assert.equal(events[0].length, 1, "should have had only 1 from first account");
-        assert.equal(events[1].length, 1, "should have had only 1 from second account");
-        assert.equal(events[0][0].args.previousOwner, accounts[0], "should be the first account");
-        assert.equal(events[0][0].args.newOwner, accounts[1], "should be the second account");
-        assert.equal(events[1][0].args.previousOwner, accounts[1], "should be the second account");
-        assert.equal(events[1][0].args.newOwner, accounts[0], "should be the first account");
-        return owned.setOwner(accounts[1], { from: accounts[0] })
-      })
-      .then(function (tx) {
-        return getEventsPromise(owned.OnOwnerChanged(
-            { previousOwner: accounts[0] },
-            { fromBlock: 0, toBlock: "latest" }),
-          2);        
-      })
-      .then(function (events) {
-        assert.equal(events.length, 2, "should have received 2 events for this previous account");
-        assert.equal(events[0].args.previousOwner, accounts[0], "should be the first account");
-        assert.equal(events[0].args.newOwner, accounts[1], "should be the second account");
-        assert.equal(events[1].args.previousOwner, accounts[0], "should be the first account");
-        assert.equal(events[1].args.newOwner, accounts[1], "should be the second account");
-        return getEventsPromise(owned.OnOwnerChanged(
-            { newOwner: accounts[0] }, // newOwner is NOT indexed
-            { fromBlock: 0, toBlock: "latest" }),
-          1);
-      })
-      .then(function (events) {
-        assert.equal(events.length, 3, "should have received all events as newOwner is not indexed");
-      });
+    before("should have proper accounts", function() {
+        assert.isAtLeast(accounts.length, 2, "should have at least 2 accounts");
+        owner = accounts[0];
+        owner2 = accounts[1];
+        return Extensions.makeSureAreUnlocked([ owner ]);
+    });
+    
+    beforeEach("should deployed a new Owned", function() {
+        return Owned.new({ from: owner })
+            .then(created => {
+                owned = created;
+                return web3.eth.getTransactionReceiptPromise(created.transactionHash);
+            })
+            .then(receipt => block0 = receipt.blockNumber);
+    });
 
-  });
+    it("should receive the changed owner", function() {
+        return owned
+            .setOwner(owner2, { from: owner })
+            .then(txObject => {
+                assert.equal(txObject.logs.length, 1, "should have received 1 event");
+                assert.equal(
+                    txObject.logs[0].args.previousOwner, accounts[0],
+                    "should be the first account");
+                assert.equal(
+                    txObject.logs[0].args.newOwner, accounts[1],
+                    "should be the second account");
+                return owned.setOwner(owner, { from: owner2 });
+            })
+            .then(txObject => {
+                assert.equal(txObject.logs.length, 1, "should have received 1 event");
+                assert.equal(
+                    txObject.logs[0].args.previousOwner, owner2,
+                    "should be the second account");
+                assert.equal(
+                    txObject.logs[0].args.newOwner, owner,
+                    "should be the first account");
+                return Promise.all([
+                        Extensions.getEventsPromise(owned.OnOwnerChanged(
+                            { previousOwner: owner },
+                            { fromBlock: block0, toBlock: txObject.receipt.blockNumber })),
+                        Extensions.getEventsPromise(owned.OnOwnerChanged(
+                            { previousOwner: owner2 },
+                            { fromBlock: block0, toBlock: txObject.receipt.blockNumber }))
+                    ]);
+            })
+            .then(events => {
+                assert.equal(events[0].length, 1, "should have had only 1 from first account");
+                assert.equal(events[1].length, 1, "should have had only 1 from second account");
+                assert.equal(
+                    events[0][0].args.previousOwner, owner,
+                    "should be the first account");
+                assert.equal(
+                    events[0][0].args.newOwner, owner2,
+                    "should be the second account");
+                assert.equal(
+                    events[1][0].args.previousOwner, owner2,
+                    "should be the second account");
+                assert.equal(
+                    events[1][0].args.newOwner, owner,
+                    "should be the first account");
+                return owned.setOwner(owner2, { from: owner })
+            })
+            .then(txObject => Extensions.getEventsPromise(owned.OnOwnerChanged(
+                    { previousOwner: owner },
+                    { fromBlock: block0, toBlock: txObject.receipt.blockNumber }),
+                2))
+            .then(events => {
+                assert.equal(events.length, 2, "should have received 2 events for this previous account");
+                assert.equal(
+                    events[0].args.previousOwner, owner,
+                    "should be the first account");
+                assert.equal(
+                    events[0].args.newOwner, owner2,
+                    "should be the second account");
+                assert.equal(
+                    events[1].args.previousOwner, owner,
+                    "should be the first account");
+                assert.equal(
+                    events[1].args.newOwner, owner2,
+                    "should be the second account");
+                return Extensions.getEventsPromise(owned.OnOwnerChanged(
+                        { newOwner: owner }, // newOwner is NOT indexed
+                        { fromBlock: block0, toBlock: events[1].blockNumber }),
+                    3);
+            })
+            .then(events => assert.equal(
+                events.length, 3, "should have received all events as newOwner is not indexed"));
+
+    });
 
 });
